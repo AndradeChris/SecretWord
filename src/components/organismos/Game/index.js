077ca81e6ce87a0aeb1e45722a614e46949073c3
Christian Andrade <andrade.dias.christian@gmail.com>
2022-06-07 22:33:37 -0300

    create game page components and style

--- src/components/organismos/Game/index.js
+++ src/components/organismos/Game/index.js
@@ -2,13 +2,33 @@ import React from 'react'
 import Button from '../../atomos/Button'
 import Instrucoes from '../../atomos/Instrucoes'
 import Title from '../../atomos/Title'
+import './index.css'
 
 const Game = ({endGame}) => {
   return (
-    <div>
-        <Title />
-        <Instrucoes />
-        <Button funcGame={endGame}>Finalizar</Button>   
+    <div className='container'>
+        <Title/>
+        <p className='points'>
+          <span>Pontos</span>
+        </p>
+        <h3 className='category-tip'>
+          Categoria da palavra: <span className='category-word'>categoria</span>
+        </h3>
+        <div className='wrong-letters-and-attemps'>
+        <p className='attemps'>Tentativas restantes</p>
+          <p className='wrong-letters'>Letras erradas:</p>
+        </div>
+        <div className="letter-game-container">
+          <span className='letter-game-card'>A</span>
+          <span className="letter-game-card-empty"></span>
+        </div>
+        <div className='input-letters-container'>
+          <form>
+            <input type='text' className='letter-input' maxLength={1} required/>
+            <button>Jogar</button>
+          </form>
+        </div>
+        <Button funcGame={endGame}>terminar</Button>  
     </div>
   )
 }

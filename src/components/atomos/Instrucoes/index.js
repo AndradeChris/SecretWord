6186e0b08d20d37adffe91b122a5a5ca3e5ccc52
Christian Andrade <christian.andrade@kroton.com.br>
2022-06-10 10:32:12 -0300

    interim adjustments in css

--- src/components/atomos/Instrucoes/index.js
+++ src/components/atomos/Instrucoes/index.js
@@ -10,9 +10,10 @@ const Instrucoes = () => {
   }
 
   const [instrucoes] = useState([
-    {id: 1, texto: 'O jogo consiste em um jogo de palavras, onde você deve adivinhar as palavras que estão escondidas no tabuleiro'},
-    {id: 2, texto: 'Para começar, clique no botão "Iniciar" e uma categoria sera escolhida aleatoriamente'},
-    {id: 3, texto: 'Você deve adivinhar as palavras que estão escondidas no tabuleiro'},
+    {id: 1, texto: 'Para começar, clique no botão "Começar" e uma categoria sera escolhida aleatoriamente'},
+    {id: 2, texto: 'Você deve adivinhar as palavras que estão escondidas no tabuleiro'},
+    {id: 3, texto: 'Você terá 5 chances para adivinhar a palavra'},
+    {id: 4, texto: 'Para ser validada, as letras devem possuir acento quando necessário'},
   ])
 
   return (
--- src/components/organismos/GameOver/index.js
+++ src/components/organismos/GameOver/index.js
@@ -1,11 +1,12 @@
 import Button from '../../atomos/Button'
 import Title from '../../atomos/Title'
+import styles from './index.module.css'
 
 const GameOver = ({resetGame, score}) => {
   return (
     <div>
         <Title customTitleProp='max-title'>GameOver</Title>
-        <h2>Sua pontuação foi: {score}</h2>
+        <h2 className={styles['final-score']}>Sua pontuação foi: {score}</h2>
         <Button funcGame={resetGame}>Resetar</Button>   
     </div>
   )
